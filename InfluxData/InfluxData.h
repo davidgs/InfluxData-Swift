e156cfbc1641db8dd056210b8c6d48414d7c15c6
David G. Simmons <santafen@mac.com>
2019-03-27 10:03:55 -0400

    Initial Commit

---
+++ InfluxData/InfluxData.h
@@ -0,0 +1,19 @@
+//
+//  InfluxData.h
+//  InfluxData
+//
+//  Created by David G. Simmons on 3/27/19.
+//  Copyright © 2019 David G. Simmons. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for InfluxData.
+FOUNDATION_EXPORT double InfluxDataVersionNumber;
+
+//! Project version string for InfluxData.
+FOUNDATION_EXPORT const unsigned char InfluxDataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InfluxData/PublicHeader.h>
+
+
---
+++ InfluxDataTests/InfluxDataTests.swift
@@ -0,0 +1,34 @@
+//
+//  InfluxDataTests.swift
+//  InfluxDataTests
+//
+//  Created by David G. Simmons on 3/27/19.
+//  Copyright © 2019 David G. Simmons. All rights reserved.
+//
+
+import XCTest
+@testable import InfluxData
+
+class InfluxDataTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
